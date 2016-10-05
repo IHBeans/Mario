f5504ac2518318210dbf1f0021469e95f288c4b6
Ianisthebest <ian.huynh@hotmail.com>
2016-10-05 03:21:35 +0100

    Initial commit

---
+++ android/src/com/ian/mario/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ian.mario.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ian.mario.MarioGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MarioGame(), config);
+	}
+}
