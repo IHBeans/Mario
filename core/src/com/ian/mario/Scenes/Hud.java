31f9d260a56e12a9b4cafbd930619abf65bad162
Ianisthebest <ian.huynh@hotmail.com>
2016-10-08 00:10:03 +0100

    test

--- core/src/com/ian/mario/Scenes/Hud.java
+++ core/src/com/ian/mario/Scenes/Hud.java
@@ -38,11 +38,20 @@ public class Hud {
         table.setFillParent(true); //makes the table the size of the stage
 
         countdownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
-        scoreLabel
-                timeLabel
-        levelLabel
-        worldLabel
-        marioLabel
+        scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        timeLabel = new Label("TIME" , new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        levelLabel = new Label("1-1" , new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        worldLabel = new Label("WORLD" , new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        marioLabel = new Label("MARIO" , new Label.LabelStyle(new BitmapFont(), Color.WHITE));
 
+        table.add(marioLabel).expandX().padTop(10);
+        table.add(worldLabel).expandX().padTop(10);
+        table.add(timeLabel).expandX().padTop(10);
+        table.row();
+        table.add(scoreLabel).expandX();
+        table.add(levelLabel).expandX();
+        table.add(countdownLabel).expandX();
+
+        stage.addActor(table);
     }
 }
--- core/src/com/ian/mario/Screens/PlayScreen.java
+++ core/src/com/ian/mario/Screens/PlayScreen.java
@@ -1,25 +1,29 @@
 package com.ian.mario.Screens;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.ian.mario.Constants;
 import com.ian.mario.MarioGame;
+import com.ian.mario.Scenes.Hud;
 
 public class PlayScreen implements Screen {
 
     private MarioGame game;
     private OrthographicCamera camera;
     private Viewport gameViewport;
+    private Hud hud;
 
     public PlayScreen(MarioGame game) {
         this.game = game;
         camera = new OrthographicCamera();
         gameViewport = new FitViewport(Constants.V_WIDTH, Constants.V_HEIGHT, camera);
-
+        hud = new Hud(game.batch);
     }
 
     @Override
@@ -29,6 +33,9 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix();
 
     }
 
